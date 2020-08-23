a4b7b3d3745f1860e4352a6af24a4a09f45e1898
Ogerboss <ogerboss@gmx.de>
2020-08-23 19:48:21 +0200

    RSE-32 - update Kotlin version, simplify Reqtificator build file

--- Java/Reqtificator/build.gradle.kts
+++ Java/Reqtificator/build.gradle.kts
@@ -1,11 +1,19 @@
 import skyrim.requiem.build.VersionFileTask
 
 plugins {
-    application
-    java
     kotlin("jvm")
+    application
     id("org.jlleitschuh.gradle.ktlint")
     id("org.beryx.jlink")
+}
+
+java {
+    modularity.inferModulePath.set(true)
+}
+
+application {
+    mainModule.set("skyrim.requiem")
+    mainClass.set("skyrim.requiem.MainKt")
 }
 
 repositories {
@@ -18,8 +26,7 @@ if (reqtificatorBuildDir != null) {
     buildDir = reqtificatorBuildDir!!.resolve(project.name)
 }
 
-val reqtificatorDir = objects.directoryProperty()
-reqtificatorDir.set(file("$rootDir/SkyProc Patchers/Requiem/app"))
+val reqtificatorDir by extra(file("$rootDir/SkyProc Patchers/Requiem/app"))
 
 dependencies {
     implementation(kotlin("stdlib-jdk8"))
@@ -27,6 +34,7 @@ dependencies {
     implementation("org.apache.logging.log4j:log4j-api:2.13.0")
     implementation("org.apache.logging.log4j:log4j-core:2.13.0")
     implementation(project(":Java:SkyProc"))
+    testImplementation(kotlin("reflect"))
     testImplementation("io.kotlintest:kotlintest-runner-junit5:3.3.2")
     testImplementation("io.mockk:mockk:1.9.3")
     testImplementation("net.bytebuddy:byte-buddy:1.10.6")
@@ -58,7 +66,6 @@ tasks.jar {
             )
         )
     }
-    duplicatesStrategy = DuplicatesStrategy.EXCLUDE
 }
 
 val cleanReqtificator = tasks.register<Delete>("cleanReqtificator") {
@@ -73,38 +80,18 @@ tasks.clean {
     dependsOn(cleanReqtificator)
 }
 
-tasks.compileKotlin {
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-    destinationDir = tasks.compileJava.map { it.destinationDir }.get()
-}
-tasks.compileTestKotlin {
-    kotlinOptions {
-        jvmTarget = "11"
-    }
-}
-
 tasks.test {
     useJUnitPlatform()
 }
 
-val moduleName by project.extra("skyrim.requiem")
-
 tasks.compileJava {
-    options.release.set(11)
-    inputs.property("moduleName", moduleName)
-    doFirst {
-        options.compilerArgs = listOf("--module-path", classpath.asPath)
-        classpath = files()
-    }
+    destinationDir = tasks.compileKotlin.map { it.destinationDir }.get()
 }
 
 jlink {
-    setProperty("mainClass", "Reqtificator.Reqtificator")
     setProperty("options", listOf("--compress", "2", "--no-header-files", "--no-man-pages"))
     setProperty("imageDir", reqtificatorDir)
-    forceMerge("log4j-api", "config")
+    forceMerge("log4j-api", "kotlin") // kotlin-stdlib-common has no module-identifier and has split modules
 
     launcher {
         name = "launcher_template"
