535ae104f0c9828a738020e313ced9a2bb0693ce
Ogerboss <ogerboss@gmx.de>
2021-05-04 23:18:29 +0200

    RRO-000 - generate mod headers and set version stamp in plugin

---
+++ components/mutagen-reqtificator/Reqtificator/Export/StaticReferences.cs
@@ -0,0 +1,9 @@
+﻿using Mutagen.Bethesda;
+
+namespace Reqtificator.Export
+{
+    public static class StaticReferences
+    {
+        public static readonly FormKey VersionStamp = FormKey.Factory("973D69:Requiem.esp");
+    }
+}
