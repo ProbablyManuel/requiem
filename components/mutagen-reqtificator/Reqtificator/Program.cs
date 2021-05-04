1aa851244d56fba29acee10441226e4544a380d0
Ogerboss <ogerboss@gmx.de>
2021-05-04 23:18:29 +0200

    RRO-000 - generate mod headers and set version stamp in plugin

---
+++ components/mutagen-reqtificator/Reqtificator/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Mutagen.Bethesda;
+using Mutagen.Bethesda.Skyrim;
+using Mutagen.Bethesda.Synthesis;
+using Reqtificator.Export;
+
+//TODO: figure out why I need to do this when adding Mutagen as a dependency
+namespace System.Runtime.CompilerServices
+{
+    internal record IsExternalInit;
+}
+
+namespace Reqtificator
+{
+    public class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            return await SynthesisPipeline.Instance
+                .AddPatch<ISkyrimMod, ISkyrimModGetter>(RunPatch)
+                .SetTypicalOpen(GameRelease.SkyrimSE, "Requiem for the Mutated.esp")
+                .Run(args);
+        }
+
+        public static void RunPatch(IPatcherState<ISkyrimMod, ISkyrimModGetter> state)
+        {
+            //TODO: refactor this into a nice verification function
+            var requiemModKey = new ModKey("Requiem", ModType.Plugin);
+
+            if (state.LoadOrder.PriorityOrder.All(x => x.ModKey != requiemModKey))
+            {
+                Console.WriteLine("oops, where's Requiem.esp? -- hit enter to abort the patcher");
+                Console.ReadLine();
+                return;
+            }
+
+            var requiem = state.LoadOrder.PriorityOrder.First(x => x.ModKey == requiemModKey);
+
+            var version = new RequiemVersion(5, 0, 0, "a Phoenix perhaps?");
+            var processor = new PatchData();
+            processor.SetPatchHeadersAndVersion(requiem.Mod!, state.PatchMod, version);
+
+            Console.WriteLine("Done! Press enter to finish your self-compiled Mutagen patcher.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ components/mutagen-reqtificator/Reqtificator/RequiemVersion.cs
@@ -0,0 +1,20 @@
+﻿namespace Reqtificator
+{
+    public record RequiemVersion(int Major, int Minor, int Patch, string Title)
+    {
+        public override string ToString()
+        {
+            return $"Requiem {Major}.{Minor}.{Patch} - \"{Title}\"";
+        }
+
+        public string ShortVersion()
+        {
+            return $"{Major}.{Minor}.{Patch}";
+        }
+
+        public int AsNumeric()
+        {
+            return Major * 10000 + Minor * 100 + Patch;
+        }
+    }
+}
