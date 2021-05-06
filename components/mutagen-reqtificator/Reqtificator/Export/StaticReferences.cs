b312281404779b7b29d3a7d0f9c407e2e26f3091
Ogerboss <ogerboss@gmx.de>
2021-05-06 23:05:25 +0200

    RRO-000 - basic pipeline to patch ammunition objects

--- components/mutagen-reqtificator/Reqtificator/Export/StaticReferences.cs
+++
@@ -1,9 +0,0 @@
-﻿using Mutagen.Bethesda;
-
-namespace Reqtificator.Export
-{
-    public static class StaticReferences
-    {
-        public static readonly FormKey VersionStamp = FormKey.Factory("973D69:Requiem.esp");
-    }
-}
