649a49d438a98f8fec39e79894cf045a339403c2
Ogerboss <ogerboss@gmx.de>
2021-05-07 22:19:13 +0200

    RRO-000 - more tests

--- components/mutagen-reqtificator/ReqtificatorTest/Transformers/AmmunitionTransformerTest.cs
+++ components/mutagen-reqtificator/ReqtificatorTest/Transformers/AmmunitionTransformerTest.cs
@@ -11,69 +11,85 @@ namespace ReqtificatorTest.Transformers
 {
     public class AmmunitionTransformerTest
     {
-        [Fact]
-        public void Should_Select_Eligible_Records()
+        public class ShouldProcess 
         {
-            var transformer = new AmmunitionTransformer();
-            var arrow = new Mock<IAmmunitionGetter>(MockBehavior.Strict);
-            var keywords = ImmutableList<IFormLinkGetter<IKeywordGetter>>.Empty;
-            var bla = StaticReferences.Keywords.NoWeaponReachRescaling.AsLink<Keyword>();
-            keywords = keywords.Add(bla);
-            arrow.Setup(mock => mock.Keywords).Returns(keywords);
-            arrow.Setup(mock => mock.Damage).Returns(42);
+            [Fact]
+            public void Should_Select_Eligible_Records()
+            {
+                var transformer = new AmmunitionTransformer();
+                var arrow = new Mock<IAmmunitionGetter>(MockBehavior.Strict);
+                var keywords = ImmutableList<IFormLinkGetter<IKeywordGetter>>.Empty;
+                var bla = StaticReferences.Keywords.NoWeaponReachRescaling.AsLink<Keyword>();
+                keywords = keywords.Add(bla);
+                arrow.Setup(mock => mock.Keywords).Returns(keywords);
+                arrow.Setup(mock => mock.Damage).Returns(42.0f);
 
-            transformer.ShouldProcess(arrow.Object).Should().BeTrue();
+                transformer.ShouldProcess(arrow.Object).Should().BeTrue();
+            }
+
+            [Fact]
+            public void Should_Select_Eligible_Records_Without_Keywords_Block()
+            {
+                var transformer = new AmmunitionTransformer();
+                var arrow = new Mock<IAmmunitionGetter>(MockBehavior.Strict);
+                ImmutableList<IFormLinkGetter<IKeywordGetter>> keywords = null;
+                arrow.Setup(mock => mock.Keywords).Returns(keywords);
+                arrow.Setup(mock => mock.Damage).Returns(42.0f);
+
+                transformer.ShouldProcess(arrow.Object).Should().BeTrue();
+            }
+
+            [Fact]
+            public void Should_Not_Select_Records_Marked_as_Already_Reqtified()
+            {
+                var transformer = new AmmunitionTransformer();
+                var arrow = new Mock<IAmmunitionGetter>(MockBehavior.Strict);
+                var keywords = ImmutableList<IFormLinkGetter<IKeywordGetter>>.Empty;
+                keywords = keywords.Add(StaticReferences.Keywords.AlreadyReqtified.AsLink<Keyword>());
+                arrow.Setup(mock => mock.Keywords).Returns(keywords);
+                arrow.Setup(mock => mock.Damage).Returns(42.0f);
+
+                transformer.ShouldProcess(arrow.Object).Should().BeFalse();
+            }
+
+            [Fact]
+            public void Should_Not_Select_Records_Marked_as_No_Damage_Scaling()
+            {
+                var transformer = new AmmunitionTransformer();
+                var arrow = new Mock<IAmmunitionGetter>(MockBehavior.Strict);
+                var keywords = ImmutableList<IFormLinkGetter<IKeywordGetter>>.Empty;
+                keywords = keywords.Add(StaticReferences.Keywords.NoDamageRescaling.AsLink<Keyword>());
+                arrow.Setup(mock => mock.Keywords).Returns(keywords);
+                arrow.Setup(mock => mock.Damage).Returns(42.0f);
+
+                transformer.ShouldProcess(arrow.Object).Should().BeFalse();
+            }
+
+            [Fact]
+            public void Should_Not_Select_Records_Without_Damage()
+            {
+                var transformer = new AmmunitionTransformer();
+                var arrow = new Mock<IAmmunitionGetter>(MockBehavior.Strict);
+                var keywords = ImmutableList<IFormLinkGetter<IKeywordGetter>>.Empty;
+                keywords = keywords.Add(StaticReferences.Keywords.NoWeaponReachRescaling.AsLink<Keyword>());
+                arrow.Setup(mock => mock.Keywords).Returns(keywords);
+                arrow.Setup(mock => mock.Damage).Returns(0f);
+
+                transformer.ShouldProcess(arrow.Object).Should().BeFalse();
+            }
         }
 
-        [Fact]
-        public void Should_Select_Eligible_Records_Without_Keywords_Block()
+        public class PatchRecord
         {
-            var transformer = new AmmunitionTransformer();
-            var arrow = new Mock<IAmmunitionGetter>(MockBehavior.Strict);
-            ImmutableList<IFormLinkGetter<IKeywordGetter>> keywords = null;
-            arrow.Setup(mock => mock.Keywords).Returns(keywords);
-            arrow.Setup(mock => mock.Damage).Returns(42);
-
-            transformer.ShouldProcess(arrow.Object).Should().BeTrue();
-        }
-
-        [Fact]
-        public void Should_Not_Select_Records_Marked_as_Already_Reqtified()
-        {
-            var transformer = new AmmunitionTransformer();
-            var arrow = new Mock<IAmmunitionGetter>(MockBehavior.Strict);
-            var keywords = ImmutableList<IFormLinkGetter<IKeywordGetter>>.Empty;
-            keywords = keywords.Add(StaticReferences.Keywords.AlreadyReqtified.AsLink<Keyword>());
-            arrow.Setup(mock => mock.Keywords).Returns(keywords);
-            arrow.Setup(mock => mock.Damage).Returns(42);
-
-            transformer.ShouldProcess(arrow.Object).Should().BeFalse();
-        }
-
-        [Fact]
-        public void Should_Not_Select_Records_Marked_as_No_Damage_Scaling()
-        {
-            var transformer = new AmmunitionTransformer();
-            var arrow = new Mock<IAmmunitionGetter>(MockBehavior.Strict);
-            var keywords = ImmutableList<IFormLinkGetter<IKeywordGetter>>.Empty;
-            keywords = keywords.Add(StaticReferences.Keywords.NoDamageRescaling.AsLink<Keyword>());
-            arrow.Setup(mock => mock.Keywords).Returns(keywords);
-            arrow.Setup(mock => mock.Damage).Returns(42);
-
-            transformer.ShouldProcess(arrow.Object).Should().BeFalse();
-        }
-
-        [Fact]
-        public void Should_Not_Select_Records_Without_Damage()
-        {
-            var transformer = new AmmunitionTransformer();
-            var arrow = new Mock<IAmmunitionGetter>(MockBehavior.Strict);
-            var keywords = ImmutableList<IFormLinkGetter<IKeywordGetter>>.Empty;
-            keywords = keywords.Add(StaticReferences.Keywords.NoWeaponReachRescaling.AsLink<Keyword>());
-            arrow.Setup(mock => mock.Keywords).Returns(keywords);
-            arrow.Setup(mock => mock.Damage).Returns(0);
-
-            transformer.ShouldProcess(arrow.Object).Should().BeFalse();
+            [Fact]
+            public void Should_Scale_Up_Damage()
+            {
+                var transformer = new AmmunitionTransformer();
+                var arrow = new Mock<IAmmunition>(MockBehavior.Strict);
+                arrow.Setup(mock => mock.Damage).Returns(25.0f);
+                arrow.SetupSet(mock => mock.Damage = 100.0f);
+                transformer.Process(arrow.Object);
+            }
         }
     }
 }
