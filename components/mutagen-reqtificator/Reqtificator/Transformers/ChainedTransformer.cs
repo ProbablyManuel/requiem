80dcd0607c92fecec9e4b6e0dc5c79458010fb4a
Ogerboss <ogerboss@gmx.de>
2021-05-29 18:21:35 +0200

    RRO-000 - test cases for ChainedTransformer type

---
+++ components/mutagen-reqtificator/Reqtificator/Transformers/ChainedTransformer.cs
@@ -0,0 +1,28 @@
+﻿using Mutagen.Bethesda;
+
+namespace Reqtificator.Transformers
+{
+    internal record ChainedTransformer<T, TI, TGetter>(Transformer<T, TI, TGetter> ThisTransform,
+        Transformer<T, TI, TGetter> NextTransform) : Transformer<T, TI, TGetter> where T : MajorRecord, TI
+        where TI : IMajorRecord, TGetter
+        where TGetter : IMajorRecordGetter
+    {
+        public override bool ShouldProcess(TGetter record)
+        {
+            return ThisTransform.ShouldProcess(record) || NextTransform.ShouldProcess(record);
+        }
+
+        public override void Process(TI record)
+        {
+            if (ThisTransform.ShouldProcess(record))
+            {
+                ThisTransform.Process(record);
+            }
+
+            if (NextTransform.ShouldProcess(record))
+            {
+                NextTransform.Process(record);
+            }
+        }
+    }
+}
--- components/mutagen-reqtificator/Reqtificator/Transformers/Transformer.cs
+++ components/mutagen-reqtificator/Reqtificator/Transformers/Transformer.cs
@@ -6,7 +6,7 @@ using Noggog;
 
 namespace Reqtificator.Transformers
 {
-    public abstract class Transformer<T, TI, TGetter> where T : MajorRecord, TI
+    internal abstract record Transformer<T, TI, TGetter> where T : MajorRecord, TI
         where TI : IMajorRecord, TGetter
         where TGetter : IMajorRecordGetter
     {
@@ -21,7 +21,7 @@ namespace Reqtificator.Transformers
                 .ToImmutableList();
         }
 
-        public T? PatchRecord(TGetter record)
+        private T? PatchRecord(TGetter record)
         {
             return LogUtils.WithRecordContextLog<T?>(record)(() =>
             {
@@ -39,39 +39,6 @@ namespace Reqtificator.Transformers
         public Transformer<T, TI, TGetter> AndThen(Transformer<T, TI, TGetter> nextOperation)
         {
             return new ChainedTransformer<T, TI, TGetter>(this, nextOperation);
-        }
-    }
-
-    class ChainedTransformer<T, TI, TGetter> : Transformer<T, TI, TGetter> where T : MajorRecord, TI
-        where TI : IMajorRecord, TGetter
-        where TGetter : IMajorRecordGetter
-    {
-        public ChainedTransformer(Transformer<T, TI, TGetter> thisTransform, Transformer<T, TI, TGetter> nextTransform)
-        {
-            _thisTransform = thisTransform;
-            _nextTransform = nextTransform;
-        }
-
-        private readonly Transformer<T, TI, TGetter> _thisTransform;
-        private readonly Transformer<T, TI, TGetter> _nextTransform;
-
-        public override bool ShouldProcess(TGetter record)
-        {
-            return _thisTransform.ShouldProcess(record) || _nextTransform.ShouldProcess(record);
-        }
-
-        public override void Process(TI record)
-        {
-            if (_thisTransform.ShouldProcess(record))
-            {
-                _thisTransform.Process(record);
-            }
-
-            //TODO: strictly speaking, this check does take into account changes made by the previous step...
-            if (_nextTransform.ShouldProcess(record))
-            {
-                _nextTransform.Process(record);
-            }
         }
     }
 }
