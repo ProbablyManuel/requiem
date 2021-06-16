944100da633137f75f1e25d0ab4d1439ed548261
Ogerboss <ogerboss@gmx.de>
2021-06-17 00:58:10 +0200

    RRO-000 - pass loaded user config to GUI constructor

--- components/mutagen-reqtificator/Reqtificator/App.xaml.cs
+++ components/mutagen-reqtificator/Reqtificator/App.xaml.cs
@@ -1,6 +1,8 @@
-﻿using Reqtificator.Gui;
+﻿using System.IO;
+using Reqtificator.Gui;
 using Serilog;
 using System.Windows;
+using Mutagen.Bethesda;
 
 namespace Reqtificator
 {
@@ -9,23 +11,39 @@ namespace Reqtificator
     /// </summary>
     public partial class App : Application
     {
+        private const GameRelease Release = GameRelease.SkyrimSE;
+        private static readonly ModKey PatchModKey = ModKey.FromNameAndExtension("Requiem for the Mutated.esp");
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
             LogUtils.SetUpLogging();
-
-            Log.Information("Starting Gui");
+            Log.Information("starting the Reqtificator");
 
             var eventQueue = new InternalEvents();
-            var backend = new Program(eventQueue);
+            var backend = new Backend(eventQueue);
 
-            MainWindowViewModel mainWindowViewModel = new(eventQueue);
+            var context = GameContext.GetRequiemContext(Release, PatchModKey);
+            var userConfig = backend.LoadAndVerifyUserSettings(context);
+
+            Log.Debug("Starting Gui");
+            MainWindowViewModel mainWindowViewModel = new(eventQueue, userConfig, context.ActiveMods);
             MainWindow window = new() { DataContext = mainWindowViewModel };
             window.Show();
 
-            Log.Information("Gui Started");
-            backend.SetUpPatcher();
+            eventQueue.PatchRequested += (_, updatedSettings) =>
+            {
+                updatedSettings.WriteToFile(Path.Combine(context.DataFolder, "Reqtificator", "UserSettings.json"));
+                var generatedPatch = backend.GeneratePatch(context, updatedSettings, Release, PatchModKey);
+                Log.Information("done patching, now exporting to disk");
+                MainLogic.WritePatchToDisk(generatedPatch, context.DataFolder);
+                Log.Information("done exporting");
+                eventQueue.PublishPatchCompleted();
+                Log.CloseAndFlush();
+            };
+
+            Log.Debug("Gui Started");
         }
     }
 }
--- components/mutagen-reqtificator/Reqtificator/InternalEvents.cs
+++ components/mutagen-reqtificator/Reqtificator/InternalEvents.cs
@@ -1,28 +1,13 @@
-﻿using Mutagen.Bethesda;
+﻿using System;
 using Reqtificator.Configuration;
-using System;
-using System.Collections.Generic;
-
 
 namespace Reqtificator
 {
-    public class InternalEvents
+    internal class InternalEvents
     {
-
-        public event EventHandler<LoadOrderSettingsEventArgs> LoadOrderSettingsChanged = delegate { };
-        public event EventHandler<OptionsEventArgs> UserOptionsChanged = delegate { };
         public event EventHandler<UserSettings> PatchRequested = delegate { };
         public event EventHandler<PatchCompleted> PatchCompleted = delegate { };
-
-        public void NotifyLoadOrderSettingsChanged(LoadOrderSettingsEventArgs eventArgs)
-        {
-            LoadOrderSettingsChanged.Invoke(this, eventArgs);
-        }
-
-        public void NotifyUserOptionsChanged(OptionsEventArgs eventArgs)
-        {
-            UserOptionsChanged.Invoke(this, eventArgs);
-        }
+        public event EventHandler<Exception> ExceptionOccured = delegate { };
 
         public void RequestPatch(UserSettings userSettings)
         {
@@ -33,38 +18,11 @@ namespace Reqtificator
         {
             PatchCompleted.Invoke(this, new PatchCompleted());
         }
-    }
 
-    public class LoadOrderSettingsEventArgs : EventArgs
-    {
-        public LoadOrderSettingsEventArgs(IList<ModKey> loadOrder, IList<ModKey> npcVisualTemplateMods,
-            IList<ModKey> raceVisualTemplateMods)
+        public void ReportException(Exception ex)
         {
-            LoadOrder = loadOrder;
-            NpcVisualTemplateMods = npcVisualTemplateMods;
-            RaceVisualTemplateMods = raceVisualTemplateMods;
+            ExceptionOccured.Invoke(this, ex);
         }
-
-        public IList<ModKey> LoadOrder { get; }
-        public IList<ModKey> NpcVisualTemplateMods { get; }
-        public IList<ModKey> RaceVisualTemplateMods { get; }
-    }
-
-    public class OptionsEventArgs : EventArgs
-    {
-        public OptionsEventArgs(bool verboseLogging, bool mergeLeveledCharacters, bool mergeLeveledLists,
-            bool openEncounterZones)
-        {
-            VerboseLogging = verboseLogging;
-            MergeLeveledCharacters = mergeLeveledCharacters;
-            MergeLeveledLists = mergeLeveledLists;
-            OpenEncounterZones = openEncounterZones;
-        }
-
-        public bool VerboseLogging { get; }
-        public bool MergeLeveledCharacters { get; }
-        public bool MergeLeveledLists { get; }
-        public bool OpenEncounterZones { get; }
     }
 
     public record PatchCompleted;
