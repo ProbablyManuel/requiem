438e047739db4fdb414c0cafc79da3b52c3bbe88
Ogerboss <ogerboss@gmx.de>
2021-06-27 22:01:18 +0200

    RRO-924 - actor scripts patching

---
+++ components/mutagen-reqtificator/Reqtificator/Transformers/Actors/ActorCommonScripts.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using Mutagen.Bethesda.Plugins.Cache;
+using Mutagen.Bethesda.Skyrim;
+using Reqtificator.StaticReferences;
+
+namespace Reqtificator.Transformers.Actors
+{
+    internal class ActorCommonScripts : TransformerV2<Npc, INpcGetter>
+    {
+        private readonly ILinkCache<ISkyrimMod, ISkyrimModGetter> _linkCache;
+        private static readonly IReadOnlyList<ScriptEntry> Scripts = new[] { new ScriptEntry { Name = "REQ_NPCData" } };
+
+        public ActorCommonScripts(ILinkCache<ISkyrimMod, ISkyrimModGetter> linkCache)
+        {
+            _linkCache = linkCache;
+        }
+
+        public override TransformationResult<Npc, INpcGetter> Process(TransformationResult<Npc, INpcGetter> input)
+        {
+            if (input.Record().FormKey == Npcs.Player.FormKey) return input;
+
+            var inheritsScripts =
+                input.Record().Configuration.TemplateFlags.HasFlag(NpcConfiguration.TemplateFlag.Script);
+            return input.Record().Template.TryResolve(_linkCache) switch
+            {
+                INpcGetter _ when inheritsScripts => input,
+                INpcGetter _ => input.Modify(r =>
+                {
+                    r.VirtualMachineAdapter ??= new VirtualMachineAdapter();
+                    r.VirtualMachineAdapter.Scripts.AddRange(Scripts);
+                }),
+                ILeveledNpcGetter _ or null => input.Modify(r =>
+                {
+                    r.VirtualMachineAdapter ??= new VirtualMachineAdapter();
+                    r.VirtualMachineAdapter.Scripts.AddRange(Scripts);
+                    r.Configuration.TemplateFlags &= ~NpcConfiguration.TemplateFlag.Script;
+                }),
+                _ => input
+            };
+        }
+    }
+}
