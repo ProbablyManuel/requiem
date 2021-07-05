d1af8b519dbfe14454b4dbcdb5854e31b89f22a8
Ogerboss <ogerboss@gmx.de>
2021-07-05 21:53:39 +0200

    RRO-923 - basic configuration parser

---
+++ components/mutagen-reqtificator/Reqtificator/Transformers/Rules/AssignmentRule.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Records;
+
+namespace Reqtificator.Transformers.Rules
+{
+    public record Assignment<TAssign>(IFormLinkGetter<TAssign> ToAssign, string SourceRule)
+        where TAssign : class, IMajorRecordGetter;
+
+    public interface IAssignmentCondition<TMajorGetter> where TMajorGetter : IMajorRecordGetter
+    {
+        public bool CheckRecord(TMajorGetter record);
+    }
+
+    internal abstract class AssignmentRule<TMajorGetter, TAssign> where TMajorGetter : IMajorRecordGetter
+        where TAssign : class, IMajorRecordGetter
+    {
+        public IReadOnlySet<IAssignmentCondition<TMajorGetter>> Conditions { get; }
+
+        public IReadOnlySet<IFormLinkGetter<TAssign>> Assignments { get; }
+
+        public IReadOnlySet<AssignmentRule<TMajorGetter, TAssign>> SubNodes { get; }
+
+        public string NodeName { get; }
+
+        protected AssignmentRule(IReadOnlySet<IAssignmentCondition<TMajorGetter>> conditions,
+            IReadOnlySet<IFormLinkGetter<TAssign>> assignments, IReadOnlySet<AssignmentRule<TMajorGetter, TAssign>> subNodes,
+            string nodeName)
+        {
+            Conditions = conditions;
+            Assignments = assignments;
+            SubNodes = subNodes;
+            NodeName = nodeName;
+        }
+
+        public IEnumerable<Assignment<TAssign>> GetAssignments(TMajorGetter record)
+        {
+            return GetAssignmentsWithSource(record, "");
+        }
+
+        private IEnumerable<Assignment<TAssign>> GetAssignmentsWithSource(TMajorGetter record, string parentRule)
+        {
+            if (Conditions.All(c => c.CheckRecord(record)))
+            {
+                var fullNodeName = string.IsNullOrEmpty(parentRule) ? NodeName : $"{parentRule}.{NodeName}";
+                var assignments = Assignments.Select(a => new Assignment<TAssign>(a, fullNodeName));
+                var subAssignments = SubNodes.SelectMany(n => n.GetAssignmentsWithSource(record, fullNodeName));
+                return assignments.ToImmutableList().AddRange(subAssignments);
+            }
+
+            return ImmutableList<Assignment<TAssign>>.Empty;
+        }
+    }
+}
---
+++ components/mutagen-reqtificator/Reqtificator/Transformers/Rules/Conditions/HasAllKeywords.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Aspects;
+using Mutagen.Bethesda.Plugins.Records;
+using Mutagen.Bethesda.Skyrim;
+
+namespace Reqtificator.Transformers.Rules.Conditions
+{
+    internal record HasAllKeywords<TMajor>
+        (IReadOnlySet<IFormLinkGetter<IKeywordGetter>> Keywords) : IAssignmentCondition<TMajor>
+        where TMajor : IMajorRecordGetter, IKeywordedGetter
+    {
+        public bool CheckRecord(TMajor record)
+        {
+            return Keywords.All(k => record.Keywords?.Contains(k) ?? false);
+        }
+    }
+}
