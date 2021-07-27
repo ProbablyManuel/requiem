9678fc96c539e85b3d83ba25fd26925de2c05f63
Ludovician <ludovician@featheredwings.com>
2021-07-27 15:47:25 +0100

    RRO-941 Wired up first messages

--- components/mutagen-reqtificator/Reqtificator/App.xaml.cs
+++ components/mutagen-reqtificator/Reqtificator/App.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.Windows;
 using System.Windows.Threading;
 using Reqtificator.Gui;
@@ -12,24 +14,33 @@ namespace Reqtificator
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
+            try
+            {
+                var logContext = new ReqtificatorLogContext(LogUtils.DefaultLogFileName);
+                Log.Information("starting the Reqtificator");
 
-            var logContext = new ReqtificatorLogContext(LogUtils.DefaultLogFileName);
-            Log.Information("starting the Reqtificator");
+                var eventQueue = new InternalEvents();
+                var dispatcher = Dispatcher.CurrentDispatcher;
 
-            var eventQueue = new InternalEvents();
-            var dispatcher = Dispatcher.CurrentDispatcher;
+                Log.Debug("Starting Gui");
+                MainWindowViewModel mainWindowViewModel = new(eventQueue);
+                MainWindow window = new() { DataContext = mainWindowViewModel };
 
-            Log.Debug("Starting Gui");
-            MainWindowViewModel mainWindowViewModel = new(eventQueue);
-            MainWindow window = new() { DataContext = mainWindowViewModel };
+                eventQueue.ExceptionOccured += (_, ex) => { OnUiThread(dispatcher, () => HandleError(ex, window)); };
+                eventQueue.PatchCompleted += (_, patchStatus) => { OnUiThread(dispatcher, () => HandlePatchCompleted(window, patchStatus)); };
 
-            eventQueue.ExceptionOccured += (_, ex) => { OnUiThread(dispatcher, () => HandleError(ex, window)); };
-            eventQueue.PatchCompleted += (_, _1) => { OnUiThread(dispatcher, () => HandlePatchCompleted(window)); };
+                window.Show();
 
-            window.Show();
-
-            var backend = new Backend(eventQueue, logContext);
-            Log.Debug("Gui Started");
+                var backend = new Backend(eventQueue, logContext);
+                Log.Debug("Gui Started");
+            }
+            catch (Exception ex)
+            {
+                Log.Error(ex.Message);
+                Log.Error(ex.StackTrace);
+                HandlePatchCompleted(null, new PatchFinished(PatchStatus.GeneralError, new List<string>() { ex.Message }.ToImmutableList()));
+                throw;
+            }
         }
 
         private static void OnUiThread(Dispatcher d, Action a)
@@ -37,14 +48,15 @@ namespace Reqtificator
             d.Invoke(a);
         }
 
-        private static void HandlePatchCompleted(MainWindow window)
+        private static void HandlePatchCompleted(MainWindow? window, PatchFinished patchStatus)
         {
-            PatchCompletedViewModel patchCompletedViewModel = new();
+
+            PatchCompletedViewModel patchCompletedViewModel = new(patchStatus);
             PatchCompletedWindow pcWindow = new() { DataContext = patchCompletedViewModel };
             patchCompletedViewModel.CloseRequested += () =>
             {
                 pcWindow.Close();
-                window.Close();
+                window?.Close();
             };
             _ = pcWindow.ShowDialog();
         }
---
+++ components/mutagen-reqtificator/Reqtificator/Gui/FlowDocumentExtensions.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Documents;
+using System.Windows.Navigation;
+
+namespace Reqtificator
+{
+    public static class FlowDocumentExtensions
+    {
+        public static Paragraph P(this FlowDocument fd, string content)
+        {
+            var p = new Paragraph(new Run(content));
+            fd?.Blocks.Add(p);
+            return p;
+        }
+
+        public static Paragraph Norm(this Paragraph p, string content)
+        {
+            var p2 = p ?? new Paragraph();
+            p2.Inlines.Add(new Run(content));
+            return p2;
+        }
+
+        public static Paragraph Bold(this FlowDocument fd, string content)
+        {
+            var fd2 = fd ?? new FlowDocument();
+            var p = new Paragraph();
+            fd2.Blocks.Add(p);
+            return p.Bold(content);
+        }
+
+        public static Paragraph Bold(this Paragraph p, string content)
+        {
+            var p2 = p ?? new Paragraph();
+            p2.Inlines.Add(new Run(content) { FontWeight = FontWeights.Bold });
+            return p2;
+        }
+        public static Paragraph Link(this Paragraph p, string content, Uri uri)
+        {
+            var p2 = p ?? new Paragraph();
+            var h = new Hyperlink(new Run(content)) { NavigateUri = uri, IsEnabled = true };
+            h.RequestNavigate += (_, e) => { OpenHyperlinkExternally(e); };
+            p2.Inlines.Add(h);
+            return p2;
+        }
+
+        private static void OpenHyperlinkExternally(RequestNavigateEventArgs e)
+        {
+            _ = Process.Start(new ProcessStartInfo()
+            {
+                FileName = e.Uri.AbsoluteUri,
+                UseShellExecute = true
+            });
+            e.Handled = true;
+        }
+
+        public static List Ul(this FlowDocument fd)
+        {
+            var fd2 = fd ?? new FlowDocument();
+            var list = new List();
+            fd2.Blocks.Add(list);
+            return list;
+        }
+
+        public static List Li(this List list, string content)
+        {
+            var list2 = list ?? new List();
+            var li = new ListItem(new Paragraph(new Run(content)));
+            list2.ListItems.Add(li);
+            return list2;
+        }
+    }
+}
