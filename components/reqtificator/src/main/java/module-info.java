c85233cd8f7cdb95697d11141bda8d773f33fa35
Ogerboss <ogerboss@gmx.de>
2021-10-17 19:34:05 +0200

    RRO-959 - move Reqtificator config files, delete SkyProc Reqtificator

--- components/reqtificator/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module skyrim.requiem {
-    requires skyproc;
-    requires org.apache.logging.log4j;
-    requires kotlin.stdlib;
-    requires org.apache.logging.log4j.core;
-    requires kotlin.stdlib.jdk8;
-    requires typesafe.config;
-}
