155c633936d6d262f1bdd246e3eb64ccbc0b50a4
Ogerboss <ogerboss@gmx.de>
2021-12-21 19:32:30 +0100

    RRO-902 - generalized template finding logic

---
+++ components/mutagen-reqtificator/Reqtificator/Transformers/ForwardDataFromTemplate.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using Mutagen.Bethesda.Plugins;
+using Mutagen.Bethesda.Plugins.Cache;
+using Mutagen.Bethesda.Plugins.Order;
+using Mutagen.Bethesda.Plugins.Records;
+using Mutagen.Bethesda.Skyrim;
+using Serilog;
+
+namespace Reqtificator.Transformers
+{
+    internal class ForwardDataFromTemplate<T, TGetter> : TransformerV2<T, TGetter>
+        where T : MajorRecord, TGetter where TGetter : class, IMajorRecordGetter
+    {
+        private readonly ILinkCache<ISkyrimMod, ISkyrimModGetter> _linkCache;
+        private readonly ImmutableDictionary<ModKey, ImmutableList<ModKey>> _masters;
+        private readonly IDataForwardingLogic<T, TGetter> _logic;
+        private readonly bool _featureActive;
+
+        public ForwardDataFromTemplate(ILinkCache<ISkyrimMod, ISkyrimModGetter> linkCache,
+            ILoadOrder<IModListing<ISkyrimModGetter>> loadOrder,
+            IDataForwardingLogic<T, TGetter> processingLogic, bool featureActive)
+        {
+            _linkCache = linkCache;
+            _logic = processingLogic;
+            _featureActive = featureActive;
+            _masters = loadOrder.PriorityOrder
+                .Select(x =>
+                    KeyValuePair.Create(x.ModKey, x.Mod!.MasterReferences.Select(y => y.Master).ToImmutableList()))
+                .ToImmutableDictionary();
+        }
+
+        public override TransformationResult<T, TGetter> Process(TransformationResult<T, TGetter> input)
+        {
+            if (!_featureActive) return input;
+
+            var lastOverride = _linkCache.ResolveContext<T, TGetter>(input.Record().FormKey);
+            var otherVersions = _linkCache.ResolveAllContexts<T, TGetter>(input.Record().FormKey).Skip(1)
+                .ToImmutableList();
+
+            if (IsTemplate(lastOverride, otherVersions)) return input;
+
+            var template = otherVersions.FirstOrDefault(x => IsTemplate(x, otherVersions));
+            if (template != null && !_logic.CheckRecordEquality(lastOverride.Record, template.Record))
+            {
+                //TODO: refine this log message if the code is used for other features in the future 
+                Log.Information($"applying visual auto-merge: {template.ModKey} (visual) & {lastOverride.ModKey} (data)");
+                return input.Modify(record => _logic.ForwardDataFromTemplate(record, template.Record));
+            }
+
+            return input;
+        }
+
+        private bool IsTemplate(IModContext<ISkyrimMod, ISkyrimModGetter, T, TGetter> thisVersion,
+            ImmutableList<IModContext<ISkyrimMod, ISkyrimModGetter, T, TGetter>> otherVersions)
+        {
+            var maybePreviousVersion =
+                otherVersions.FirstOrDefault(x => _masters[thisVersion.ModKey].Contains(x.ModKey));
+
+            if (maybePreviousVersion == null) return false;
+
+            return !_logic.CheckRecordEquality(thisVersion.Record, maybePreviousVersion.Record);
+        }
+    }
+
+    public interface IDataForwardingLogic<in T1, in T1Getter>
+    {
+        public bool CheckRecordEquality(T1Getter reference, T1Getter other);
+
+        public void ForwardDataFromTemplate(T1 record, T1Getter templateRecord);
+    }
+}
