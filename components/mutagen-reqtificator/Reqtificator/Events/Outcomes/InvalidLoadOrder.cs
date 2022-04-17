28b6cc3aba09f2680f89ab59b08faa401d904d2f
Ogerboss <ogerboss@gmx.de>
2022-04-17 15:28:50 +0200

    RRO-992 - more details for "missing masters" scenario

---
+++ components/mutagen-reqtificator/Reqtificator/Events/Outcomes/InvalidLoadOrder.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections.Immutable;
+using System.Linq;
+using Mutagen.Bethesda.Plugins;
+using Reqtificator.Utils;
+
+namespace Reqtificator.Events.Outcomes
+{
+    public class InvalidLoadOrder : ReqtificatorOutcome
+    {
+        public ModKey Mod { get; }
+        public IImmutableSet<ModKey> OutOfOrderMasters { get; }
+        public IImmutableSet<ModKey> MissingMasters { get; }
+        public IImmutableSet<ModKey> DeactivatedMasters { get; }
+        public override PatchStatus Status => PatchStatus.ERROR;
+        public override string Title => "Load order validation failed!";
+
+        public override string Message
+        {
+            get
+            {
+                string FormatModList(string title, IImmutableSet<ModKey> mods)
+                {
+                    if (mods.Count == 0) return "";
+                    {
+                        var text = $@"|
+                        |
+                        |{title}
+                        |
+                        |
+                        " + mods
+                            .OrderBy(e => e.FileName.String)
+                            .Select(e => $"|* {e.FileName}\r\n").Aggregate((a, b) => a + b);
+                        return Formatter.FormatMultiline(text, '|');
+                    }
+                }
+
+                return $"Your load order could not be verified. The mod '{Mod.FileName}' has missing masters:\r\n"
+                    + FormatModList("**Missing Mods** (no file found)", MissingMasters)
+                    + FormatModList("**Disabled Mods** (file found, but not activated)", DeactivatedMasters)
+                    + FormatModList("**Unsorted Masters** (plugin loaded after depending mod)", OutOfOrderMasters);
+            }
+        }
+
+        public InvalidLoadOrder(ModKey mod, IImmutableSet<ModKey> outOfOrderMasters,
+            IImmutableSet<ModKey> missingMasters, IImmutableSet<ModKey> deactivatedMasters)
+        {
+            Mod = mod;
+            OutOfOrderMasters = outOfOrderMasters;
+            MissingMasters = missingMasters;
+            DeactivatedMasters = deactivatedMasters;
+        }
+    }
+}
