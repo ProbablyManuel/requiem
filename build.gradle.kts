e13b05232a7469b2dbf892436585a98f3bc4fce2
thetrader <nowack.manuel@gmail.com>
2022-05-23 16:32:35 +0200

    RRO-000 - version stamps for 5.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ try {
 }
 
 allprojects {
-    version = RequiemVersion(5, 2, 0, "Bits and Pieces")
+    version = RequiemVersion(5, 1, 1, "From Past to Present")
 }
 
 fun runCommand(command: List<String>): String = try {
