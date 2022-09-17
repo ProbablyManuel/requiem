f208616df8b754b2ad09de940ad8e6713f1d9c6e
ProbablyManuel <25909324+ProbablyManuel@users.noreply.github.com>
2022-09-17 22:33:28 +0200

    Update LinkScripts.py

--- tools/xEdit Scripts/LinkScripts.py
+++ tools/xEdit Scripts/LinkScripts.py
@@ -3,8 +3,13 @@ import os
 import sys
 
 
-if len(sys.argv) != 2:
-    raise ValueError('Path to xEdit installation is missing')
+try:
+    xEdit = sys.argv[1]
+except IndexError:
+    xEdit = "C:\\Skyrim Tools\\SSEEdit"
 
-for file in glob.iglob("REQ_*"):
-    os.link(file, os.path.join(sys.argv[1], "Edit Scripts", file))
+for f in glob.iglob("REQ_*"):
+    src = f
+    dst = os.path.join(xEdit, "Edit Scripts", f)
+    if not os.path.exists(dst) or not os.path.samefile(src, dst):
+        os.link(src, dst)
