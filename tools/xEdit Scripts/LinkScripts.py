a90556ddee70eb66009b1e5d3252de6892deac6d
ProbablyManuel <25909324+ProbablyManuel@users.noreply.github.com>
2022-09-29 21:00:53 +0200

    Display exception in terminal

--- tools/xEdit Scripts/LinkScripts.py
+++ tools/xEdit Scripts/LinkScripts.py
@@ -4,12 +4,15 @@ import sys
 
 
 try:
-    xEdit = sys.argv[1]
-except IndexError:
-    xEdit = "C:\\Skyrim Tools\\SSEEdit"
+    try:
+        xEdit = sys.argv[1]
+    except IndexError:
+        xEdit = "C:\\Skyrim Tools\\SSEEdit"
 
-for f in glob.iglob("REQ_*"):
-    src = f
-    dst = os.path.join(xEdit, "Edit Scripts", f)
-    if not os.path.exists(dst) or not os.path.samefile(src, dst):
-        os.link(src, dst)
+    for f in glob.iglob("REQ_*"):
+        src = f
+        dst = os.path.join(xEdit, "Edit Scripts", f)
+        if not os.path.exists(dst) or not os.path.samefile(src, dst):
+            os.link(src, dst)
+except Exception as e:
+    input(e)
