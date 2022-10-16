ca0a650f95a8c4209a8dd648a0c1ce439619522b
ProbablyManuel <25909324+ProbablyManuel@users.noreply.github.com>
2022-10-16 11:51:38 +0200

    Rekindle links broken by switching branches in git

--- tools/xEdit Scripts/link_scripts.py
+++ tools/xEdit Scripts/link_scripts.py
@@ -1,3 +1,4 @@
+import filecmp
 import glob
 import os
 import sys
@@ -12,7 +13,13 @@ try:
     for f in glob.iglob("REQ_*"):
         src = f
         dst = os.path.join(xEdit, "Edit Scripts", f)
-        if not os.path.exists(dst) or not os.path.samefile(src, dst):
+        if not os.path.exists(dst):
+            os.link(src, dst)
+        # If src and dst do not map to the same file but have the same file
+        # content, recreate the link on the assumption that it was previously
+        # established and subsequently destroyed by switching branches in git
+        elif not os.path.samefile(src, dst) and filecmp.cmp(src, dst):
+            os.remove(dst)
             os.link(src, dst)
 except Exception as e:
     input(e)
