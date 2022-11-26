d7de8184751315fb7dd3996551dd5a6784838cb7
ProbablyManuel <25909324+ProbablyManuel@users.noreply.github.com>
2022-11-26 15:41:15 +0100

    Update link_scripts.py

--- tools/xEdit Scripts/link_scripts.py
+++ tools/xEdit Scripts/link_scripts.py
@@ -15,11 +15,18 @@ try:
         dst = os.path.join(xEdit, "Edit Scripts", f)
         if not os.path.exists(dst):
             os.link(src, dst)
-        # If src and dst do not map to the same file but have the same file
-        # content, recreate the link on the assumption that it was previously
-        # established and subsequently destroyed by switching branches in git
-        elif not os.path.samefile(src, dst) and filecmp.cmp(src, dst):
-            os.remove(dst)
-            os.link(src, dst)
+        elif not os.path.samefile(src, dst):
+            # If src and dst have the same file content, recreate the link on
+            # the assumption that it was previously established and
+            # subsequently destroyed by switching branches in git
+            if filecmp.cmp(src, dst):
+                os.remove(dst)
+                os.link(src, dst)
+            else:
+                answer = input(f'{dst} already exists. '
+                               f'Do you want to overwrite this file? (y/n)\n')
+                if answer == "y":
+                    os.remove(dst)
+                    os.link(src, dst)
 except Exception as e:
     input(e)
