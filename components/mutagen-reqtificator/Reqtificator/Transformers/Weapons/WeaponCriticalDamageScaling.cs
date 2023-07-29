927371b1b4393554cc24a86fd5123475390c47a6
ProbablyManuel <25909324+ProbablyManuel@users.noreply.github.com>
2023-07-29 17:14:33 +0200

    Smarter critical damage

---
+++ components/mutagen-reqtificator/Reqtificator/Transformers/Weapons/WeaponCriticalDamageScaling.cs
@@ -0,0 +1,22 @@
+﻿using Mutagen.Bethesda.Skyrim;
+using Serilog;
+
+namespace Reqtificator.Transformers.Weapons
+{
+    internal class WeaponCriticalDamageScaling : Transformer<Weapon, IWeapon, IWeaponGetter>
+    {
+        public override TransformationResult<Weapon, IWeaponGetter> Process(
+            TransformationResult<Weapon, IWeaponGetter> input)
+        {
+            if (input.Record().Critical is null || input.Record().Template.IsNotNull())
+            {
+                return input;
+            }
+            return input.Modify(r =>
+            {
+                r.Critical!.Damage = r.BasicStats!.Damage;
+                Log.Debug("adjusted weapon critical damage");
+            });
+        }
+    }
+}
