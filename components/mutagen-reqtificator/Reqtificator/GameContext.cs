3f972d087150bd29bcb4f231e402ff2a0cf47d15
ProbablyManuel <25909324+ProbablyManuel@users.noreply.github.com>
2023-07-29 17:29:33 +0200

    Master limit experiment

--- components/mutagen-reqtificator/Reqtificator/GameContext.cs
+++ components/mutagen-reqtificator/Reqtificator/GameContext.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Immutable;
+﻿using System;
+using System.Collections.Immutable;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using Mutagen.Bethesda;
@@ -14,12 +16,22 @@ namespace Reqtificator
             return PluginListings.TryGetListingsFile(release, out _);
         }
 
-        public static GameContext GetRequiemContext(GameRelease release, ModKey patchToBeGenerated)
+        public static GameContext GetRequiemContext(GameRelease release, Collection<ModKeyStruct> patchesToBeGenerated)
         {
+            if (patchesToBeGenerated == null)
+            {
+                throw new ArgumentNullException(nameof(patchesToBeGenerated));
+            }
             string dataFolder = Directory.GetCurrentDirectory();
 
+            var modKeys = new Collection<ModKey>();
+            foreach (var modKey in patchesToBeGenerated)
+            {
+                modKeys.Add(modKey.PluginKey);
+            }
+
             var loadOrderEntries = LoadOrder.GetLoadOrderListings(release, dataFolder, true);
-            var activeMods = loadOrderEntries.OnlyEnabled().TakeWhile(it => it.ModKey != patchToBeGenerated)
+            var activeMods = loadOrderEntries.OnlyEnabled().TakeWhile(it => !modKeys.Contains(it.ModKey))
                 .ToImmutableList();
 
             return new GameContext(activeMods, dataFolder, release);
