36464a6d6f2fc85be80576980359044f20088572
ProbablyManuel <25909324+ProbablyManuel@users.noreply.github.com>
2025-08-10 14:14:52 +0200

    Derandomize in separate step

--- components/plugins/spriggit.bat
+++ components/plugins/spriggit.bat
@@ -1,2 +1,5 @@
-Spriggit.CLI sort-randomized-fields --InputPath %1 --OutputPath %1 --GameRelease SkyrimSE
-Spriggit.CLI convert-from-plugin --InputPath %1 --OutputPath components/plugins/%1
+mkdir "components/plugins/temp"
+copy /y %1 "components/plugins/temp/"%1
+Spriggit.CLI sort-randomized-fields --InputPath components/plugins/temp/%1 --OutputPath components/plugins/temp/%1 --GameRelease SkyrimSE
+Spriggit.CLI convert-from-plugin --InputPath components/plugins/temp/%1 --OutputPath components/plugins/%1
+rmdir /S /Q "components/plugins/temp"
