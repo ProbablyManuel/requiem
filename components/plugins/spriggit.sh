0ef08dec103baff45ad305bd65297df4d26e8f7f
Probably Manuel <25909324+ProbablyManuel@users.noreply.github.com>
2025-11-01 12:45:09 +0200

    Update Spriggit to v0.40.0 (#454)

--- components/plugins/spriggit.sh
+++ components/plugins/spriggit.sh
@@ -2,9 +2,4 @@
 
 set -euo pipefail
 
-tmp_dir="$(mktemp -d)"
-mkdir -p "${tmp_dir}"
-cp "$1" "${tmp_dir}/$1"
-Spriggit.CLI sort-randomized-fields --InputPath "${tmp_dir}/$1" --OutputPath "${tmp_dir}/$1" --GameRelease SkyrimSE
-Spriggit.CLI convert-from-plugin --InputPath "${tmp_dir}/$1" --OutputPath "components/plugins/$1"
-rm -rf "${tmp_dir}"
+Spriggit.CLI convert-from-plugin --InputPath "$1" --OutputPath "components/plugins/$1"
